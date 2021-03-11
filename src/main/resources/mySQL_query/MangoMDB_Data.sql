d3df64697cee9878e38ca4b234d66c3995cb0e87
mizugakun <mizugakun@gmail.com>
2021-03-11 15:13:30 -0500

    Update data query

--- src/main/resources/mySQL_query/MangoMDB_Data.sql
+++ src/main/resources/mySQL_query/MangoMDB_Data.sql
@@ -1,4 +1,3 @@
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
@@ -6,9 +5,10 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 USE MangoMDB;
 
 SET AUTOCOMMIT=0;
-INSERT INTO User VALUE(
+
+INSERT INTO User VALUES
     (1, 'Sam Kuo', 'account1', 'samkuo'),
     (2, 'Joan Wu', 'account2', 'jaonwu'),
     (3, 'Zongyao Li', 'account3', 'zongyaoli'),
-    (4, 'Yichen Hao', 'account4', 'yichenhao'));
+    (4, 'Yichen Hao', 'account4', 'yichenhao');
 COMMIT;

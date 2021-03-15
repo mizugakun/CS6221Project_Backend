1a9cb400935d78e41d157c4886892d58f3364ba7
mizugakun <mizugakun@gmail.com>
2021-03-15 12:21:16 -0400

    Edit repositories: wrong key types

--- src/main/java/com/mangomanagement/demo/dao/OrderDetailRepository.java
+++ src/main/java/com/mangomanagement/demo/dao/OrderDetailRepository.java
@@ -1,9 +1,10 @@
 package com.mangomanagement.demo.dao;
 
 import com.mangomanagement.demo.Entity.OrderDetail;
+import com.mangomanagement.demo.Entity.OrderDetailId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, Integer>, JpaSpecificationExecutor<OrderDetail> {
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, OrderDetailId>, JpaSpecificationExecutor<OrderDetail> {
 
 }

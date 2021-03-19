32851588cfbaf2804d98e4a81ade9ee43d732210
joan <joanwu11226@gmail.com>
2021-03-18 22:26:29 -0400

    SignUp function

---
+++ src/main/java/com/mangomanagement/demo/REST/SignUpController.java
@@ -0,0 +1,23 @@
+package com.mangomanagement.demo.REST;
+
+import com.mangomanagement.demo.Entity.User;
+import com.mangomanagement.demo.Service.UserService;
+import com.mangomanagement.demo.blo.UserSignUp;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/signup")
+public class SignUpController {
+    private final UserService userService;
+
+    public SignUpController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public void singUp(@RequestBody User user){
+        userService.save(user);
+    }
+
+}
---
+++ src/main/java/com/mangomanagement/demo/blo/UserSignUp.java
@@ -0,0 +1,14 @@
+package com.mangomanagement.demo.blo;
+
+public class UserSignUp {
+
+    String username;
+    String account;
+    String password;
+
+    public UserSignUp(String username,String account, String password){
+        this.username=username;
+        this.account=account;
+        this.password=password;
+    }
+}
